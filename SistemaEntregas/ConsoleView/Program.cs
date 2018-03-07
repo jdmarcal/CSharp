cef413e87406a60193adb22f0254e21b6e4650d4
Dalbert Marçal <dalbertmarcal@gmail.com>
2018-03-07 14:14:28 -0300

    Projetos

---
+++ SistemaEntregas/ConsoleView/Program.cs
@@ -0,0 +1,119 @@
+﻿using Modelos;
+using System;
+
+namespace ConsoleView
+{
+    class Program
+    {
+        enum OpcoesMenuPrincipal
+
+        {
+            CadastrarCliente,
+            PesquisarCliente,
+            EditarCliente,
+            ExcluirCliente,
+            LimparTela,
+            Sair
+        }
+
+
+
+
+
+        private static OpcoesMenuPrincipal Menu()
+        {
+            Console.WriteLine("Escolha sua opcao");
+            Console.WriteLine("");
+
+            Console.WriteLine(" - Clientes - ");
+            Console.WriteLine("1 - Cadastrar Novo");
+            Console.WriteLine("2 - Pesquisar Cliente");
+            Console.WriteLine("3 - Editar Cliente");
+
+            Console.WriteLine(" - Geral -");
+            Console.WriteLine("4 - Limpar Tela");
+            Console.WriteLine("5 - Sair");
+
+            //return Convert.ToInt32(Console.ReadLine());
+            string opcao = Console.ReadLine();
+            return (OpcoesMenuPrincipal) int.Parse(opcao);
+        }
+        static void Main(string[] args)
+        {
+            OpcoesMenuPrincipal opcaoDigitada = 
+                OpcoesMenuPrincipal.Sair;
+
+            do
+            {
+                opcaoDigitada = Menu();
+
+                switch (opcaoDigitada)
+                {
+                    case OpcoesMenuPrincipal.CadastrarCliente:
+                        CadastrarCliente();
+                        break;
+                    case OpcoesMenuPrincipal.PesquisarCliente:
+                        PesquisarCliente();
+                        break;
+                    case OpcoesMenuPrincipal.EditarCliente:
+                        break;
+                    case OpcoesMenuPrincipal.ExcluirCliente:
+                        break;
+                    case OpcoesMenuPrincipal.LimparTela:
+                        break;
+                    case OpcoesMenuPrincipal.Sair:
+                        break;
+                    default:
+                        break;
+                }
+               
+            } while (opcaoDigitada != OpcoesMenuPrincipal.Sair);
+            
+        }
+
+        // Metodos Cliente
+        private static Cliente CadastrarCliente()
+        {
+            Cliente cli = new Cliente();
+
+            Console.Write("Digite o nome: ");
+            cli.Nome = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.Write("Digite o cpf: ");
+            cli.Cpf = Console.ReadLine();
+
+            // ... Endereco
+            Endereco end = new Endereco();
+
+            Console.Write("Digite o nome da rua: ");
+            end.Rua = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.Write("Digite o numero: ");
+            end.Numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Digite o complemento: ");
+            end.Complemento = Console.ReadLine();
+
+            cli._Endereco = end;
+
+            return cli;
+        }
+
+        private static Cliente PesquisarCliente()
+        {
+            // TODO : Fazer depois
+            return new Cliente();
+        }
+
+        private static void ExibirDadosCliente(Cliente cliente)
+        {
+
+        }
+    }
+}
